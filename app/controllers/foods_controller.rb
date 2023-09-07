17e37fab6c333bc12779f218e5ed86df9ac2e12b
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-07 12:24:10 +0100

    Add create controller action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,14 +7,14 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
-  # def create
-  #   user = current_user
-  #   food = user.foods.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
-  #   if food.save
-  #     redirect_to '/foods', notice:"Food created successfully"
-  #   else
-  #     flash[:alert] = "An error occured when creating the food"
-  #     redirect_to '/foods'
-  #   end
-  # end
+  def create
+    user = current_user
+    food = user.foods.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+    if food.save
+      redirect_to '/foods', notice:"Food created successfully"
+    else
+      flash[:alert] = "An error occured when creating the food"
+      redirect_to '/foods'
+    end
+  end
 end
