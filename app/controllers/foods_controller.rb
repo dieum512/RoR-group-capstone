4930a1164d50fdaeaba7de7b0bc3efeb2fefd32f
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-07 12:23:49 +0100

    Add new food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,19 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  # def create
+  #   user = current_user
+  #   food = user.foods.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+  #   if food.save
+  #     redirect_to '/foods', notice:"Food created successfully"
+  #   else
+  #     flash[:alert] = "An error occured when creating the food"
+  #     redirect_to '/foods'
+  #   end
+  # end
 end
