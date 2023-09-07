1a8dec7f22df404d788b771c20b8d96ce81b8f80
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-07 12:40:14 +0100

    Fix linters issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,9 +11,9 @@ class FoodsController < ApplicationController
     user = current_user
     food = user.foods.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
     if food.save
-      redirect_to '/foods', notice:"Food created successfully"
+      redirect_to '/foods', notice: 'Food created successfully'
     else
-      flash[:alert] = "An error occured when creating the food"
+      flash[:alert] = 'An error occured when creating the food'
       redirect_to '/foods'
     end
   end
