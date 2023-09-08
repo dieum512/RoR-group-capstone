6e85ed672fe551e911e663865cba87dd88a118be
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-08 22:16:10 +0100

    Add general shopping list algorithm

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,7 +1,27 @@
 class GeneralShoppingListController < ApplicationController
   def index
-    flash[:alert] = 'You are not connected'
     redirect_to '/public_recipes' unless user_signed_in?
-    @foods = Food.all
+    @total_value = 0
+    @recipe_foods = []
+    recipes = current_user.recipes
+    recipes.each do |recipe|
+
+      new_recipe_foods = recipe.recipe_foods.includes(:food).select do |recipe_food|
+        food = recipe_food.food
+        user_food = current_user.foods.find_by(name: food.name, measurement_unit: food.measurement_unit)
+        @total_value += recipe_food.process_cost(user_food)
+        recipe_food.process_quantity(user_food).positive?
+        p "total of #{food.name} buy: quantity:#{recipe_food.process_quantity(user_food)} number: #{recipe_food.process_cost(user_food)}"
+      end
+
+      @recipe_foods = @recipe_foods + new_recipe_foods
+    end
+
+    # recipe = Recipe.find(params[:recipe_id])
+    # @total_value = @total_value.round(2)
+    @items_to_buy =  @recipe_foods.count
+
+
+    # @foods = Food.all
   end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,15 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  def process_quantity(user_food)
+    return @quantity_needed = quantity unless user_food
+
+    diff = user_food.quantity - quantity
+    @quantity_needed = diff.negative? ? -diff : 0
+  end
+
+  def process_cost(user_food)
+    @cost_required = (process_quantity(user_food) * food.price).round(2)
+  end
 end
