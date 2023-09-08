c0e83d4dc1a9ceb9093a55f8777c459232f2dbff
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-08 12:25:10 +0100

    Redirect users to public recipes if the are not connected

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,26 @@
 class RecipesController < ApplicationController
   def index
+    flash[:alert] = "You are not connected"
+    redirect_to '/public_recipes' unless user_signed_in?
     @recipes = Recipe.all
   end
 
   def show
+    flash[:alert] = "You are not connected"
+    redirect_to '/public_recipes' unless user_signed_in?
     @recipe = Recipe.find(params[:id])
     @foods = Food.all
   end
 
   def new
+    flash[:alert] = "You are not connected"
+    redirect_to '/public_recipes' unless user_signed_in?
     @recipe = Recipe.new
   end
 
   def create
+    flash[:alert] = "You are not connected"
+    redirect_to '/public_recipes' unless user_signed_in?
     @user = current_user
     @recipe = @user.recipes.new(recipe_params)
     if @recipe.save
