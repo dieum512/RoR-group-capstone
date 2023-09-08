2cf4e2dee8bfe0b2a712550417ed4cb2aa4fa729
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-08 20:59:56 +0100

    Add new ingredient adding feature

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,26 @@
+class RecipeFoodsController < ApplicationController
+
+  def new
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def create
+    redirect_to '/public_recipes' unless user_signed_in?
+    user = current_user
+    food = user.foods.new(params.require(:recipe_food).permit(:name, :measurement_unit, :price, :quantity))
+    if food.save
+      @recipe = Recipe.find(params[:recipe_id])
+      recipe_food = RecipeFood.new(recipe: @recipe, food: food)
+      if recipe_food.save
+        redirect_to "/recipes/#{params[:recipe_id]}", notice: 'Food created successfully'
+      else
+        flash[:alert] = 'An error occured when creating the recipe food'
+        redirect_to "/recipes/#{params[:recipe_id]}"
+      end
+    else
+      flash[:alert] = 'An error occured when creating the recipe food'
+      redirect_to "/recipes/#{params[:recipe_id]}"
+    end
+  end
+end
