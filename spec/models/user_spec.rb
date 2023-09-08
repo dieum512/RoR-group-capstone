9441fb20e78aa7dffd93874ba22c8ebf2217f012
dieum512 <akonkwadieum222@gmail.com>
2023-09-08 22:49:13 +0200

    Test the food model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    it 'Has many foods' do
+        association = described_class.reflect_on_association(:foods)
+        expect(association.macro).to eq(:has_many)
+    end
+
+    it 'Has many recipes' do
+        association = described_class.reflect_on_association(:recipes)
+        expect(association.macro).to eq(:has_many)
+    end
+end
